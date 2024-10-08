c8a23f3ecc994eb3a2c5cbdf86e20ca7f4c8b529
Sleipnir <sleipnircrypto@protonmail>
2024-10-09 01:09:26 +0200

    Refactored Docs Repo utilizing Nextra

---
+++ pages/_meta.js
@@ -0,0 +1,22 @@
+export default {
+    "index": {
+        "title": "Introduction",
+        "order": 1
+    },
+    "Game-Strategy": {
+        "title": "Game Strategy",
+        "order": 2
+    },
+    "GamePlay": {
+        "title": "Gameplay",
+        "order": 3
+    },
+    "TraitForger-Entity": {
+        "title": "TraitForger Entity",
+        "order": 4
+    },
+    "FAQs": {
+        "title": "FAQs",
+        "order": 5
+    }
+};
