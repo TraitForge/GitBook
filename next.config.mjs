ec425a740b7082558565267fafc50c75513c7a0b
Thomas Hunt <albertderr9978@gmail.com>
2024-10-11 13:35:47 +0930

    Updates

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-import nextra from 'nextra';
-
-const withNextra = nextra({
-    latex: true,
-    theme: 'nextra-theme-docs',
-    themeConfig: './theme.config.mjs'
-});
-export default withNextra();
